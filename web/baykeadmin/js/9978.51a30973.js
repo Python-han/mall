d4125d10844077ceadb9e5f6a43c5aba670bcdb4
轻编程 <2539909370@qq.com>
2023-08-23 14:51:39 +0000

    build: 打包后端前端项目

---
+++ web/baykeadmin/js/9978.51a30973.js
@@ -0,0 +1,1 @@
+"use strict";(self["webpackChunkbaykeshop"]=self["webpackChunkbaykeshop"]||[]).push([[9978],{9978:function(e,n,r){r.r(n),r.d(n,{default:function(){return o}});var u=r(6252);function t(e,n){const r=(0,u.up)("router-view");return(0,u.wg)(),(0,u.j4)(r)}var s=r(3744);const a={},c=(0,s.Z)(a,[["render",t]]);var o=c}}]);
